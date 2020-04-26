ae5e4019df455137b10e522da8f1886d826b0c01
Libida <220790Libida>
2020-04-26 19:45:25 +0300

    Task 1 was implemented

---
+++ src/server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8080;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+app.listen(port, () => console.log(`React app listening at http://localhost:${port}`));

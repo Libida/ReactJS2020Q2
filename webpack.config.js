bcb3ae3aceffb0242d85784d2112b60b3060affd
Maryia Sinkevich <maryia_sinkevich@epam.com>
2020-05-09 20:02:49 +0300

    [task 2] fixed issues with build tasks after code review

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,44 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const devMode = process.env.NODE_ENV !== "production";
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-module.exports = {
-    entry: ['./src/client/index.jsx'],
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: "[name].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
+module.exports = (env, argv) => {
+    const isDevMod = argv.mode === 'production';
+
+    return {
+        entry: ['./src/client/index.jsx'],
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: "[name].js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"]
                 }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+            ]
+        },
+        devServer: {
+            port: 3000,
+            open: true,
+            proxy: {
+                "/api": "http://localhost:8080" // For potential future use with SSR
             }
-        ]
-    },
-    devServer: {
-        port: 3000,
-        open: true,
-        proxy: {
-            "/api": "http://localhost:8080" // For potential future use with SSR
-        }
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./src/client/index.html"
-        })
-    ],
-    devtool: devMode ? 'source-map' : 'none'
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: "./src/client/index.html"
+            })
+        ],
+        devtool: isDevMod ? 'source-map' : 'none'
+    }
 };
